f707b55d2d2a549591a1f42273c50fb1c626e786
nicksolony <nicksolony@gmail.com>
2020-05-25 22:59:01 -0400

    person creation and viewing is added

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,18 +1,19 @@
 class PeopleController < ApplicationController
   def index
-
+    @people=Person.all.sort_by(&:name)
   end
 
   def new
-
+    @person=Person.new
   end
 
   def create
-
+    @person=Person.create(person_params)
+    redirect_to person_path(@person.slug)
   end
 
   def show
-
+    @person=Person.find_by(:slug=> params[:id])
   end
 
   def edit
@@ -22,4 +23,10 @@ class PeopleController < ApplicationController
   def destroy
 
   end
+
+  private
+  def person_params
+    params.require(:person).permit(:name,:dob)
+  end
+
 end
