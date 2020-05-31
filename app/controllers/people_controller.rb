fddeff16c456dcdd23cabd8e0bd6254d73e720c2
nicksolony <nicksolony@gmail.com>
2020-05-31 14:44:42 -0400

    added filmomgraphy

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,8 @@
 class PeopleController < ApplicationController
   def index
-    @people=Person.all.sort_by(&:name)
+    binding.pry
+    @people=Person.order(:name)
+    @directors=Person.where(directed_movies: true)
   end
 
   def new
@@ -14,6 +16,9 @@ class PeopleController < ApplicationController
 
   def show
     if person_identify
+      @characters=@person.characters_played
+      @written_movies=@person.written_movies
+      @directed_movies=@person.directed_movies
     else
       redirect_to people_path
     end
@@ -43,6 +48,18 @@ class PeopleController < ApplicationController
     if person_identify
       if !@person.written_movies.empty?
         @movies=@person.written_movies
+      else
+        redirect_to person_path(@person.slug)
+      end
+    else
+      redirect_to people_path
+    end
+  end
+
+  def filmography
+    if person_identify
+      if !@person.characters_played.empty?
+        @characters=@person.characters_played
       else
         redirect_to person_path(@person.slug)
       end
